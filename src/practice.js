f4155cd32b566c13039700bc8f820a02ef58debc
Eman Curmi <emancurmi@gmail.com>
2020-06-22 11:53:16 -0400

    update

---
+++ src/practice.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+    client: 'pg',
+    connection: process.env.DB_URL
+})
+
+function getProductsWithImages() {
+    knexInstance
+        .select('product_id', 'name', 'price', 'category', 'image')
+        .from('amazong_products')
+        .whereNotNull('image')
+        .then(result => {
+            console.log(result)
+        })
+}
+
+getProductsWithImages()
